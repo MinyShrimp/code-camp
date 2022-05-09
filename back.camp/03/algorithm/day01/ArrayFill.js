5ded74b9e280d9a31c99ac5207bc30a07874e2c1
MinyShrimp <ksk7584@gmail.com>
2022-05-09 14:03:10 +0900

    Algorithm : Day 1

---
+++ back.camp/03/algorithm/day01/ArrayFill.js
@@ -0,0 +1,12 @@
+// 핸드폰 번호가 담긴 문자열 phone이 주어질 때,
+// 뒤의 4자리를 "*"로 바꿔서 리턴해주세요.
+
+function solution(phone) {
+    let arr = [...phone], length = phone.length;
+    for (let i = phone.length - 1; i > phone.length - 5; i--) {
+        arr[i] = "*";
+    }
+    return arr.join("");
+}
+
+console.log("결과", solution("01012345678")); // "0101234****"
