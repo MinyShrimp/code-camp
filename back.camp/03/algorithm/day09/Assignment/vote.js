a066c4473048b8e20a3a830c6249ed106952c229
MinyShrimp <ksk7584@gmail.com>
2022-05-10 15:12:49 +0900

    Algorithm : Day 09 Finish

--- back.camp/03/algorithm/day09/Assignment/vote.js
+++ back.camp/03/algorithm/day09/Assignment/vote.js
@@ -1,10 +1,18 @@
 //assignment
 function vote(str) {
     // 아래에 코드를 작성해주세요.
+    let answer = new Map();
 
-    let answer;
-
-    return answer;
+    [...str].forEach(v => {
+        if( [...answer.keys()].includes(v) ) {
+            answer.set( v, answer.get(v) + 1 );
+        } else {
+            answer.set( v, 1 );
+        }
+    });
+    
+    const max = Math.max( ...answer.values() );
+    return [...answer.entries()].filter(v => v[1] === max)[0][0];
 }
 
 // 아래의 코드는 절대로 수정하거나 삭제하지 마세요.
