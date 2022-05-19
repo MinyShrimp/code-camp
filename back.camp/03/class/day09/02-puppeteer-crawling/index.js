d093424de5252c58f2fb8d82f574ce8706acb282
MinyShrimp <ksk7584@gmail.com>
2022-05-19 18:07:44 +0900

    Class : Day 09 : 2교시 수정

--- back.camp/03/class/day09/02-puppeteer-crawling/index.js
+++ back.camp/03/class/day09/02-puppeteer-crawling/index.js
@@ -8,10 +8,10 @@ import puppeteer from "puppeteer";
  */
 const url = "https://www.goodchoice.kr/product/search/2";
 const selectors = {
-    card:     "#poduct_list_area > li:nth-child(2)",
-    star:     "#poduct_list_area > li:nth-child(2) > a > div > div.name > div > span",
-    title:    "#poduct_list_area > li:nth-child(2) > a > div > div.name > strong",
-    price:    "#poduct_list_area > li:nth-child(2) > a > div > div.price > p > b",
+    card: "#poduct_list_area > li:nth-child(2)",
+    star: "#poduct_list_area > li:nth-child(2) > a > div > div.name > div > span",
+    title: "#poduct_list_area > li:nth-child(2) > a > div > div.name > strong",
+    price: "#poduct_list_area > li:nth-child(2) > a > div > div.price > p > b",
     location: "#poduct_list_area > li:nth-child(2) > a > div > div.name > p:nth-child(4)",
 };
 
@@ -30,16 +30,18 @@ const startCrawling = async () => {
     await page.goto(url);
 
     // 1초 대기
-    await page.waitForTimeout( 1000 );
+    await page.waitForTimeout(1000);
 
     // selector를 이용해 textContent 가져오기
-    const title    = page.$eval(selectors.title,    (el) => el.textContent.trim());
-    const star     = page.$eval(selectors.star,     (el) => el.textContent.trim());
-    const price    = page.$eval(selectors.price,    (el) => el.textContent.trim());
-    const location = page.$eval(selectors.location, (el) => el.textContent.trim());
+    const title = page.$eval(selectors.title, (el) => el.textContent.trim());
+    const star = page.$eval(selectors.star, (el) => el.textContent.trim());
+    const price = page.$eval(selectors.price, (el) => el.textContent.trim());
+    const location = page.$eval(selectors.location, (el) =>
+        el.textContent.trim()
+    );
 
-    console.log( await title, await star, await price, await location );
-}
+    console.log(await title, await star, await price, await location);
+};
 
 /**
  * Main Function
