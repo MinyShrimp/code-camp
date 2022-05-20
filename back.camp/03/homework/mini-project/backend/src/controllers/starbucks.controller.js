3c38fbee66dcbda6e445a13d261bad04932f53f5
MinyShrimp <ksk7584@gmail.com>
2022-05-20 09:10:47 +0900

    주석 추가

--- back.camp/03/homework/mini-project/backend/src/controllers/starbucks.controller.js
+++ back.camp/03/homework/mini-project/backend/src/controllers/starbucks.controller.js
@@ -1,6 +1,7 @@
 
 import Starbucks from "../models/starbucks.model.js";
 
+// 모두 찾기
 export const getAllMenu = async () => {
     const datas = await Starbucks.find();
     return datas;
--- back.camp/03/homework/mini-project/backend/src/controllers/user.controller.js
+++ back.camp/03/homework/mini-project/backend/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 
 import User from "../models/user.model.js";
 
+// 유저 새로 만들기
 export const createUser = async ( user ) => {
     const newUser = new User( user );
     await newUser.save();
@@ -8,6 +9,7 @@ export const createUser = async ( user ) => {
     return newUser._id;
 }
 
+// 모든 유저 찾기
 export const getAllUser = async ( ) => {
     const result = await User.find();
     return result;
