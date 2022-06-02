dd2fcefc60f3bb856b2c4c9db5ef1adb45a578ed
MinyShrimp <ksk7584@gmail.com>
2022-06-02 23:18:53 +0900

    MainProject : 주석추가

--- back.camp/03/homework/main-project/src/apis/productCategory/productCategory.module.ts
+++ back.camp/03/homework/main-project/src/apis/productCategory/productCategory.module.ts
@@ -14,6 +14,7 @@ import ProductCategoryService from './productCategory.service';
         ]),
     ],
     exports: [
+        // To. Product Category Search
         ProductCategoryService, //
     ],
     providers: [
