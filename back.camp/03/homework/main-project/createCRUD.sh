ddb415f96907ec987cd38e0808cff4cfeb1df5e5
MinyShrimp <ksk7584@gmail.com>
2022-06-03 10:32:46 +0900

    MainProject : Add 기본 구조

--- back.camp/03/homework/main-project/createCRUD.sh
+++ back.camp/03/homework/main-project/createCRUD.sh
@@ -6,12 +6,12 @@ BASEDIR=./copy/base
 APIDIR=./src/apis/$FILENAME
 UPPER="$(tr '[:lower:]' '[:upper:]' <<< ${FILENAME:0:1})${FILENAME:1}"
 
-cp -R $BASEDIR $APIDIR
+cp -R $BASEDIR/* $APIDIR
 mv $APIDIR/base.module.ts $APIDIR/$FILENAME.module.ts
 mv $APIDIR/base.resolver.ts $APIDIR/$FILENAME.resolver.ts
 mv $APIDIR/base.service.ts $APIDIR/$FILENAME.service.ts
 
-mv $APIDIR/entities/base.entity.ts $APIDIR/entities/$FILENAME.entity.ts
+# mv $APIDIR/entities/base.entity.ts $APIDIR/entities/$FILENAME.entity.ts
 
 mv $APIDIR/dto/createBase.input.ts $APIDIR/dto/create$UPPER.input.ts
 mv $APIDIR/dto/updateBase.input.ts $APIDIR/dto/update$UPPER.input.ts
