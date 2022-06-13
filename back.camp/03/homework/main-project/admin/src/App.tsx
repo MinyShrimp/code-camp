2f61d56658b4b02be8106afa0d392c40306212a4
MinyShrimp <ksk7584@gmail.com>
2022-06-13 23:49:34 +0900

    MainProject : Add Admin Page By React

--- back.camp/03/homework/main-project/admin/src/App.tsx
+++ back.camp/03/homework/main-project/admin/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './App.css';
+import MainPage from 'containers/MainPage';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <MainPage></MainPage>
+        </div>
+    );
 }
 
 export default App;
